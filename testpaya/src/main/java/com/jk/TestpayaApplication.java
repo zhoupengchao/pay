09e9467cd2279479a47a39ede5d9be4edf42b707
zpc <403657717@qq.com>
2018-12-17 16:32:57 +0800

    创建支付模块

---
+++ testpaya/src/main/java/com/jk/TestpayaApplication.java
@@ -0,0 +1,14 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestpayaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestpayaApplication.class, args);
+    }
+
+}
+
